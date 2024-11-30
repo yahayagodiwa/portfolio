d20fb8521efcc88b7f8c3e769fe17e04ef235c81
yahayagodiwa <yahayagodiwa8@gmail.com>
2024-11-30 20:36:13 +0100

    worked on the nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ export const NavBar = () => {
     setShowNav(!showNav)
   }
   return (
-    <nav className=" h-[38px] pb-14 z-10 pt-6 px-5 md:px-20 fixed top-0 left-0 right-0 w-[100%] bg-[#203350] overflow-x-hidden">
+    <nav className=" h-[38px] pb-14 z-10 pt-6 px-5 md:px-20 fixed top-0 left-0 right-0  bg-[#203350]">
 <div className=" text-white flex justify-between md:items-center ">
     <h1 className="text-xl font-bold">Yahya <span className="text-[#FF6347]">Godiwa</span> </h1>
    {/* ////////////////// */}
@@ -17,8 +17,8 @@ export const NavBar = () => {
     <FiMenu onClick={handleClick} className="md:hidden text-2xl"/>
     <ul className={`${showNav? "hidden": "flex-col text-center" } md:flex md:flex-row gap-8 md:gap-10
      md:items-center
-    fixed md:static top-20 left-0  md:w-auto 
-    bg-[#203350]  transition-all duration-300 w-full z-40`}>
+    absolute md:static top-20 left-0 right-0 md:w-auto 
+    bg-[#203350]  transition-all duration-300 max-w-full z-40`}>
 
       {/* ////////// */}
        <a href="#about"> <li className="hover:bg-[#A5D7E86B] hover:px-4 hover:rounded-lg cursor-pointer py-1">About</li></a>
